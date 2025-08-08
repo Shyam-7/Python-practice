67d30e5150cc92100bd7ef0c5f6548513152f169
CSS <css@CSSs-MacBook-Pro.local>
2025-08-08 14:52:55 +0530

    python basics

---
+++ datatypes.py
@@ -0,0 +1,3 @@
+# Numeric data types
+a: int = 20
+print(a)
---
+++ functions.py
@@ -0,0 +1,9 @@
+#functions.py using type hints, defining various functions and their return types
+
+#defining a function
+
+def function_name():
+    print("this is the body of the function")
+
+function_name()
+
